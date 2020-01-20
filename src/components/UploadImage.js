ad5b88f5804c899c94175ead1ec9484577fa6c51
EllieChen-Git <elliechen.etc@gmail.com>
2020-01-20 12:20:30 +1100

    added uploaded image

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class UploadImage extends Component {
+  state = {
+    selectedFile: null
+  };
+
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  };
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append("image", this.state.selectedFile, this.state.selectedFile.name);
+    axios
+      .post("{url}", fd, {
+        onUploadProgress: progressEvent => {
+          console.log(
+            "Upload Progress: " +
+              Math.round((progressEvent.loaded / progressEvent.total) * 100) +
+              "%"
+          );
+        }
+      })
+      .then(res => {
+        console.log(res);
+      });
+  };
+
   render() {
-    return <h1>Upload Image</h1>;
+    return (
+      <>
+        <h1>Upload Image</h1>
+        <div className="App">
+          <input
+            style={{ display: "none" }}
+            type="file"
+            onChange={this.fileSelectedHandler}
+            ref={fileInput => (this.fileInput = fileInput)}
+          />
+          <button onClick={() => this.fileInput.click()}>Choose File</button>
+          <button onClick={this.fileUploadHandler}>Upload</button>
+        </div>
+      </>
+    );
   }
 }
 
