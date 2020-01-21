1cfdb96460d1e3747a2ff44f8997b4a4be195e41
Ellie <elliechen.etc@gmail.com>
2020-01-21 20:57:29 +1100

    Fixed 'title' issue by separating titleHandler & urlHandler

--- src/components/UploadImageFile.js
+++ src/components/UploadImageFile.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class UploadImageFile extends Component {
   state = {
@@ -9,11 +10,27 @@ class UploadImageFile extends Component {
     this.setState({
       selectedFile: event.target.files[0]
     });
+    // console.log(this.state.selectedFile);
   };
 
   fileUploadHandler = event => {
     event.preventDefault();
-    console.log(this.state.selectedFile.name);
+    const data = new FormData();
+    data.append("file", this.state.selectedFile);
+    // console.log(this.state.selectedFile);
+    axios
+      .post("http://localhost:5000/images", data, {
+        onUploadProgress: progressEvent => {
+          console.log(
+            "Upload Progress: " +
+              Math.round((progressEvent.loaded / progressEvent.total) * 100) +
+              "%"
+          );
+        }
+      })
+      .then(res => {
+        console.log(res);
+      });
   };
   render() {
     return (
@@ -21,7 +38,11 @@ class UploadImageFile extends Component {
         <form onSubmit={this.fileUploadHandler}>
           <label>
             Image:
-            <input type="file" onChange={this.fileSelectedHandler} />
+            <input
+              type="file"
+              name="file"
+              onChange={this.fileSelectedHandler}
+            />
           </label>
           <input type="submit" value="Upload File" />
         </form>
