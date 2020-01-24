c6a654ec06fd4947ea1ebb4fcb70d711e58ecf99
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-24 11:18:15 +1100

    added in react routing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,31 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import ImageAnnotation from "./components/ImageAnnotation";
 import UploadImageFile from "./components/UploadImageFile";
 import NavBar from "./components/shared/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./components/shared/Footer";
+import LandingPage from "./components/pages/LandingPage"; 
+import { BrowserRouter, Route } from "react-router-dom";
+import RegisterForm from "./components/forms/RegisterForm";
 
 
 class App extends Component {
   render() {
-    return (
-      <Router>
+      return (
         <div>
-          <NavBar />
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/annotate">Annotate</Link>
-            </li>
-            <li>
-              <Link to="/images">Manage Images</Link>
-            </li>
-          </ul>
-          <hr />
-          <Switch>
-            <Route exact path="/">
-              <h1>Welcome</h1>
-            </Route>
-            <Route path="/annotate">
-              <ImageAnnotation />
-            </Route>
-            <Route path="/images">
-              <h1>List of appartments</h1>
-              <UploadImageFile />
-            </Route>
-          </Switch>
-          <Footer />
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={NavBar} />
+                    <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/register" component={RegisterForm} />
+                    <Route path="/" component={Footer} />
+                </div>
+            </BrowserRouter>
         </div>
-      </Router>
+
     );
   }
 }
 
 export default App;
+
---
+++ src/components/pages/LandingPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <img src="landingpageimage.png" alt="What's up"/>
+                <h1>Company</h1>
+                    <p>SkyChute, is a Sydney based technology company that makes software for the property industry. SkyChutes' software, is breaking new ground and changing the way technology and the building industry relates</p>
+                <h2>Purpose</h2>
+                    <p>The purpose of this website is image annotation. We hope that through the use of this software, various professionals within the construction industry will be better able to price their inventory of units and properties. This will enable developers and other intrested parties to solve problems relating to how best to price their stock.</p>
+            </div>
+        )
+    }
+}
+
+export default LandingPage; 
