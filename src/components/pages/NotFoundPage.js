eb9132ecdb16d7528c4ddaeb730cbc882bc98fe8
Ellie <elliechen.etc@gmail.com>
2020-01-27 11:13:41 +1100

    Changed public image structure & added code in NotFoundPage

---
+++ src/components/pages/NotFoundPage.js
@@ -0,0 +1,15 @@
+//Copied from Garret's template
+
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>Sorry but we could not find the page you were looking for.</p>
+      <p>Check the url and try again.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
