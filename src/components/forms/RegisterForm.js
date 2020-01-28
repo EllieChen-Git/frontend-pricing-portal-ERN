2c57f6f546d67fcad6f7a1ad7047c8611188ffdd
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-28 16:04:38 +1100

    frontend register

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
-import axios from "axios"; 
+import LocalApi from '../../apis/LocalApi'
 
 class RegisterForm extends Component {
+  
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        const{username, email, password} = this.state
+        LocalApi.post("users", { username, email, password })
+       .then(response => {
+         // Redirect to login page for now.
+         this.props.history.push("/signin");
+       })
+       .catch(error => {
+           console.log(error);
+           this.setState({ error });
+       });
+    }
 
-        onFormSubmit = (event) => {
-            event.preventDefault();
-            const { username, email, password } = this.state;
-        
-            axios.post("http://localhost:5000/", { username, email, password })
-                .then(response => console.log(response))
-                .catch(err => console.log(err));
-        };
+    onInputChange = (name, event) => {
+        this.setState({ [name]: event.target.value });
+    };    
 
     render() {
         return (
             <div>
-                <form>
-                Username:
-                <input type="text" />
-                Email:
-                <input type="email" />
-                Password:
-                <input type="password" />
-                <input type="submit" value="Register" onChange={this.onFormSubmit}/>
+                <form onClick={this.handleClick} onSubmit={this.onFormSubmit}>
+                    <label>Username:</label>
+                    <input type="text" onChange={e => this.onInputChange('username', e)} />
+                    <label>Email:</label>
+                    <input type="email" onChange={e => this.onInputChange('email', e)} />
+                    <label>Password:</label>
+                    <input type="password" onChange= {e => this.onInputChange('password', e)} />
+                    <input type="submit" value="Register"/>
                 </form>
                 <h2>All ready have an account with Skychute?</h2>
                 <a href="/signin">Sign In</a>
             </div>
             );
-  }
-}
+        }
+};
+
+
 
 export default RegisterForm; 
