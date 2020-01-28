8efd927c6ff3e605a3db4c6e8a7cf8ae433b8fa1
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-29 08:41:18 +1100

    puedocode for SiginForm

--- src/components/forms/SignInForm.js
+++ src/components/forms/SignInForm.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import LocalApi from '../../apis/LocalApi'
 
 class SigninForm extends Component {
-
+//we create a class. 
+//we create an event handler. 
     onFormSubmit = (event) => {
+//we prevent the default behaviour of our event, 
         event.preventDefault();
+//we create a variable that contains the state of our username and password. 
         const {username, password} = this.state
-
+//we use axios through our import of LocalApi, to post to our backend the username and password. 
         LocalApi.post("/users/login", { username, password })
+//if we successfully post to our backend we then return to the home page. 
        .then(response => {
          // Redirect to login page for now.
          console.log(response)
          this.props.history.push("/");
        })
+//otherwise, we console.log what our error is. 
        .catch(error => {
            console.log(error);
            console.log("not working")
            this.setState({ error });
        });
     }
-
+//we create an event handler that takes two arguments name and event and put it through a function
+//we set the value of our state to the events with the users input for username and password. 
     onInputChange = (name, event) => {
         this.setState({ [name]: event.target.value });
     };    
