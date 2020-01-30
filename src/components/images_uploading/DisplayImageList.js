42b106b206217c11bb1326ae0f0e758576e390a9
INRokh <ilnarnika@gmail.com>
2020-01-30 22:27:11 +1100

    Assign user to annotation

--- src/components/images_uploading/DisplayImageList.js
+++ src/components/images_uploading/DisplayImageList.js
@@ -1,7 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, useReducer } from "react";
 import { connect } from "react-redux";
 import { fetchImages, fetchUsers } from "./../../actions";
 import { Button } from "react-bootstrap";
+import LocalApi from "../../apis/LocalApi";
+
+
+class UserAssignmentDropDown extends React.Component {
+  state = {value: "unknown"};
+
+  handleChange = (e) => this.setState({value: e.target.value});
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    LocalApi.post("annotations/",
+      {image_id: this.props.imageId, user_id: this.state.value})
+      .then(res => this.state.value="unknown")
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const {users} = this.props;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="unknown">Choose one</option>
+          {users && users.map(user => {
+            return (
+              <option key={user._id} value={user._id}>{user.username}</option>
+            );
+          })}
+        </select>
+        {this.state.value !== "unknown" && // Show submit buttion only when selected a user.
+          <input type="submit" value="Assign" />}
+      </form>
+    );
+  }
+}
 
 class DisplayImageList extends Component {
   componentDidMount() {
@@ -15,25 +50,10 @@ class DisplayImageList extends Component {
       <ul>
         {images.map(image => {
           return (
-            <>
               <li key={image._id}>
-                Unit Number:
-                {image.unitNumber}
-                <Button
-                  className="m-2"
-                  href={`${process.env.REACT_APP_BASEURL}/images/${image.s3key}`}
-                >
-                  Show Floor Plan
-                </Button>
-                {/* The button below is currently doing nothing! */}
-                <Button
-                  className="m-2"
-                  href={`${process.env.REACT_APP_BASEURL}/images/${image.s3key}/annotate`}
-                >
-                  Add Annotation
-                </Button>
+                {image.lot} {image.unitNumber} {image.productDescription}
+                <UserAssignmentDropDown imageId={image._id} users={users} />
               </li>
-            </>
           );
         })}
       </ul>
