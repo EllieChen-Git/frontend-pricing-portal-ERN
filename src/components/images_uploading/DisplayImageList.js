da50578854e9643124ff9d8b813ffecfebba53d2
Ellie <elliechen.etc@gmail.com>
2020-01-30 22:24:45 +1100

    Fix multiple bugs

--- src/components/images_uploading/DisplayImageList.js
+++ src/components/images_uploading/DisplayImageList.js
@@ -14,25 +14,23 @@ class DisplayImageList extends Component {
       <ul>
         {images.map(image => {
           return (
-            <>
-              <li key={image._id}>
-                Unit Number:
-                {image.unitNumber}
-                <Button
-                  className="m-2"
-                  href={`${process.env.REACT_APP_BASEURL}/images/${image.s3key}`}
-                >
-                  Show Floor Plan
-                </Button>
-                {/* The button below is currently doing nothing! */}
-                <Button
-                  className="m-2"
-                  href={`${process.env.REACT_APP_BASEURL}/images/${image.s3key}/annotate`}
-                >
-                  Add Annotation
-                </Button>
-              </li>
-            </>
+            <li key={image._id}>
+              Unit Number:
+              {image.unitNumber}
+              <Button
+                className="m-2"
+                href={`${process.env.REACT_APP_BASEURL}/images/${image.s3key}`}
+              >
+                Show Floor Plan
+              </Button>
+              {/* The button below is currently doing nothing! */}
+              <Button
+                className="m-2"
+                href={`${process.env.REACT_APP_BASEURL}/images/${image.s3key}/annotate`}
+              >
+                Add Annotation
+              </Button>
+            </li>
           );
         })}
       </ul>
--- src/components/images_uploading/UploadImageFile.js
+++ src/components/images_uploading/UploadImageFile.js
@@ -17,19 +17,20 @@ class UploadImageFile extends Component {
     const { lot, selectedFile, unitNumber, productDescription } = this.state;
     const formData = new FormData();
 
-    //At this moment, if create apt without attaching an img, the app will break. Need to fix it later.
-    for (let x = 0; x < selectedFile.length; x++) {
-      formData.append("file", selectedFile[x]);
-      formData.append("lot", lot);
-      formData.append("unitNumber", unitNumber);
-      formData.append("productDescription", productDescription);
+    if (selectedFile) {
+      for (let x = 0; x < selectedFile.length; x++) {
+        formData.append("file", selectedFile[x]);
+        formData.append("lot", lot);
+        formData.append("unitNumber", unitNumber);
+        formData.append("productDescription", productDescription);
+      }
+
+      console.log(this.state); //Keep this line to see if upload successful in browser console
+
+      LocalApi.post("/images", formData).then(res => {
+        console.log(res.statusText);
+      });
     }
-
-    console.log(this.state); //Keep this line to see if upload successful in browser console
-
-    LocalApi.post("/images", formData).then(res => {
-      console.log(res.statusText);
-    });
   };
 
   fileSelectedHandler = event => {
@@ -46,48 +47,44 @@ class UploadImageFile extends Component {
     const { lot, unitNumber, productDescription } = this.state;
 
     return (
-      <>
-        <form onSubmit={this.fileUploadHandler}>
-          <div>
-            <label htmlFor="lot">Lot: </label>
-            <input
-              type="text"
-              value={lot}
-              name="lot"
-              onChange={event => this.onInputChange("lot", event)}
-            />
-          </div>
-          <div>
-            <label htmlFor="unitNumber">Unit Number: </label>
-            <input
-              type="text"
-              value={unitNumber}
-              name="unitNumber"
-              onChange={event => this.onInputChange("unitNumber", event)}
-            />
-          </div>
-          <div>
-            <label htmlFor="productDescription">Product Description: </label>
-            <input
-              type="text"
-              value={productDescription}
-              name="productDescription"
-              onChange={event =>
-                this.onInputChange("productDescription", event)
-              }
-            />
-          </div>
-          <div>
-            <label>
-              Image:
-              <input type="file" multiple onChange={this.fileSelectedHandler} />
-            </label>
-          </div>
-          <div>
-            <input type="submit" value="Upload File" />
-          </div>
-        </form>
-      </>
+      <form onSubmit={this.fileUploadHandler}>
+        <div>
+          <label htmlFor="lot">Lot: </label>
+          <input
+            type="text"
+            value={lot}
+            name="lot"
+            onChange={event => this.onInputChange("lot", event)}
+          />
+        </div>
+        <div>
+          <label htmlFor="unitNumber">Unit Number: </label>
+          <input
+            type="text"
+            value={unitNumber}
+            name="unitNumber"
+            onChange={event => this.onInputChange("unitNumber", event)}
+          />
+        </div>
+        <div>
+          <label htmlFor="productDescription">Product Description: </label>
+          <input
+            type="text"
+            value={productDescription}
+            name="productDescription"
+            onChange={event => this.onInputChange("productDescription", event)}
+          />
+        </div>
+        <div>
+          <label>
+            Image:
+            <input type="file" multiple onChange={this.fileSelectedHandler} />
+          </label>
+        </div>
+        <div>
+          <input type="submit" value="Upload File" />
+        </div>
+      </form>
     );
   }
 }
