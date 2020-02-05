f8349c0cc1a526a61a6c6d31154e697acd615c5a
INRokh <ilnarnika@gmail.com>
2020-02-05 11:33:06 +1100

    Added styles in AnnotationList.

--- src/components/annotations/AnnotationList.js
+++ src/components/annotations/AnnotationList.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchAnnotations } from "./../../actions";
-import { Container, ListGroup, Button } from "react-bootstrap";
+import { Container, ListGroup, Button, Row, Col, Badge } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+const statusColourSchema = {
+  NEW: "success",
+  IN_PROGRESS: "warning",
+  REVIEW: "light",
+  COMPLETED: "light"
+};
+
+const statusShowOrder = {
+  NEW: 2,
+  IN_PROGRESS: 1,
+  REVIEW: 3,
+  COMPLETED: 4
+}
 
 class AnnotationList extends Component {
   componentDidMount() {
@@ -11,32 +25,37 @@ class AnnotationList extends Component {
 
   render() {
     const { annotations } = this.props;
-    // console.log(this.props);
+    annotations.sort((l, r) => {
+      return statusShowOrder[l.status] - statusShowOrder[r.status];
+    });
     return (
       <Container>
-        <h1 className="text-center">Apartments Assigned to Me</h1>
-        <ListGroup>
-          {annotations &&
-            annotations.map(annotation => {
-              const image = annotation.image_id;
-              // const image = annotation._id; //Ellie
-              console.log(image);
-
-              //need to change to 'const image = annotation._id;'
-              //when we assign image to users, it didn't pass to users
-              return (
-                <ListGroup.Item key={annotation._id}>
-                  <div> Lot: {image.lot}</div>
-                  <div> Unit Number: {image.unitNumber}</div>
-                  <div> Product Description:{image.productDescription}</div>
-                  <div> Status:{annotation.status}</div>
-                  <Link to={"annotations/" + annotation._id}>
-                    <Button variant="success"> Go to Annotation</Button>
-                  </Link>
-                </ListGroup.Item>
-              );
-            })}
-        </ListGroup>
+        <Row>
+          <Col>
+            <h1>Apartments Assigned to Me</h1>
+            <ListGroup>
+              {annotations &&
+                annotations.map(annotation => {
+                  const image = annotation.image_id;
+                  return (
+                    <ListGroup.Item
+                      key={annotation._id}
+                    >
+                      <Badge variant={statusColourSchema[annotation.status]} style={{fontSize: "1.5em"}}>
+                        {annotation.status}
+                      </Badge>
+                      <h3> Lot: {image.lot} </h3>
+                      <h3>Unit: {image.unitNumber}</h3>
+                      <h5>Product Description: {image.productDescription}</h5>
+                      <Link to={"annotations/" + annotation._id} style={{fontSize: "1.5em"}}>
+                        Go to Annotation
+                      </Link>
+                    </ListGroup.Item> 
+                  );
+                })}
+            </ListGroup>
+          </Col>
+        </Row>
       </Container>
     );
   }
